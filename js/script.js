60fbfbaa3b8d6b90ede7dfa3bacebd4e07ef2afb
venerafrontend <ashirbaevna85@gmail.com>
2023-06-12 11:20:03 +0300

    new section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-
-
-let swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    });
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2.5,
+            spaceBetween: 50,
+        },
+    },
+});
